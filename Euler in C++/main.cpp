3f25b4f405690cabf554c3856d41ef7cb1419a7e
kafkaesc <allthosemoments@gmail.com>
2015-11-25 11:32:47 -0600

    Initial Commit

---
+++ Euler in C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euler in C++
+//
+//  Created by Jared Hettinger on 11/25/15.
+//  Copyright © 2015 Openquote Design. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
